cb7c3a9d30570a12656d718738c70b8e16133ad5
Konstantin <argentum.konstantinium@gmail.com>
2021-09-21 02:11:40 +0300

    init

---
+++ src/components/Popup.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="popup">
+    <div class="popup__start">
+
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Popup',
+
+}
+</script>
+
+
+<style scoped lang="scss">
+</style>
