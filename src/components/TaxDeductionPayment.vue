2ab446b9bfc4251d0446733f287bb4e4728907d8
Konstantin <argentum.konstantinium@gmail.com>
2021-09-21 10:19:14 +0300

    feat: create payment component

---
+++ src/components/TaxDeductionPayment.vue
@@ -0,0 +1,47 @@
+<template>
+  <label class="payment">
+    <BasicCheckbox @change="$emit('select', {remove: $event, payment})" class="payment__checkbox"/>
+    <p>
+      <span class="payment__number">{{ payment.payment }}</span>
+      <span class="payment__date">
+        {{ payment.paymentDate }}
+      </span>
+    </p>
+  </label>
+</template>
+
+<script>
+import BasicCheckbox from '@/components/basic/BasicCheckbox'
+
+export default {
+  name: 'TaxDeductionPayment',
+  components: {
+    BasicCheckbox
+  },
+  props: {
+    payment: {
+      type: Object,
+      default() {
+        return {}
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.payment {
+  display: flex;
+  align-items: center;
+  padding: 16px 0;
+  cursor: pointer;
+
+  &__date {
+    color: #808080;
+  }
+
+  &__checkbox {
+    margin-right: 0.714em;
+  }
+}
+</style>
