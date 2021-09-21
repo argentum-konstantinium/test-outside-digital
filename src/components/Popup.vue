50fa24b1b59ab7fec431d78d36e31e438c126593
Konstantin <argentum.konstantinium@gmail.com>
2021-09-21 10:17:49 +0300

    feat: create popup component

--- src/components/Popup.vue
+++ src/components/Popup.vue
@@ -1,18 +1,64 @@
 <template>
-  <div class="popup">
-    <div class="popup__start">
-
-    </div>
+  <div @click.self="closePopup" :class="{ 'popup_active': popupOpened }" class="popup">
+    <transition name="show">
+      <BasicButton v-if="!popupOpened" @click="openPopup" class="popup__start-btn" type="outline">
+        Налоговый вычет
+      </BasicButton>
+      <TaxDeductionPopup v-else @close="closePopup" />
+    </transition>
   </div>
 </template>
 
 <script>
+import BasicButton from '@/components/basic/BasicButton'
+import TaxDeductionPopup from "@/components/TaxDeductionPopup";
 export default {
   name: 'Popup',
-
+  components: {
+    TaxDeductionPopup,
+    BasicButton
+  },
+  data() {
+    return {
+      popupOpened: false
+    }
+  },
+  methods: {
+    openPopup() {
+      this.popupOpened = true
+    },
+    closePopup() {
+      this.popupOpened = false
+    }
+  }
 }
 </script>
 
 
-<style scoped lang="scss">
+<style lang="scss">
+@import "@/assets/style/variables.scss";
+@import "@/assets/style/animations.scss";
+.popup {
+  background: $background-red;
+  box-shadow: $box-shadow;
+  min-height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  transition: all 0.3s ease-in;
+  &_active {
+    background: #0000004C;
+  }
+  &__start-btn {
+    flex-basis: 149px;
+  }
+}
+@media(min-width: 768px) {
+  .popup {
+    &__start-btn {
+      flex-basis: 200px;
+      min-width: 200px;
+    }
+  }
+}
 </style>
